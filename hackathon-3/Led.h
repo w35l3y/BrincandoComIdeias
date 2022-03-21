dc98afea0274f4de08e421bc336d73d92a887fb5
w35l3y <w35l3y@brasnet.org>
2022-03-21 12:39:51 -0300

    Update Led.h

--- hackathon-3/Led.h
+++ hackathon-3/Led.h
@@ -5,14 +5,16 @@
  * que funcione com sinal digital HIGH (ligado) ou LOW (desligado).
  * Por exemplo, você pode fazer com que um buzzer ativo bipe N vezes usando "blink".
 */
-#define LED_BLINK_MODE 0
-#define LED_STATIC_MODE 1
+#define LED_STATIC_MODE 0
+#define LED_BLINK_MODE_FINITE 1
+#define LED_BLINK_MODE_INFINITE 2
 
 class Led {
   private:
     unsigned mode = LED_STATIC_MODE;
     bool lastState = false;
     bool lastStateBeforeBlink = false;
+    bool inverted = false;
     unsigned int pin;
     unsigned int times = 0;
     unsigned long statusOn = 0L;
@@ -21,23 +23,28 @@ class Led {
 
     void localTurn(bool state);
   public:
-    Led(unsigned int pin);
+    Led(unsigned int pin, bool inverted = false);
     void turn(bool state);
     void turnOn();
     void turnOff();
-    void blink(unsigned int times = 1, unsigned long statusOn = 100, unsigned long statusOff = 100);
+    void play();
+    void stop();
+    void blink(unsigned int times = 0, unsigned long statusOn = 100L, unsigned long statusOff = 100L);
     void loop();
 };
 
-Led::Led(unsigned int pin) {
+Led::Led(unsigned int pin, bool inverted) {
   pinMode(pin, OUTPUT);
   this->pin = pin;
+  this->inverted = inverted;
+  bool state = digitalRead(pin);
+  this->lastStateBeforeBlink = this->lastState = (inverted ? !state : state);
 }
 
 void Led::localTurn(bool state) {
   this->lastStatus = millis();
   this->lastState = state;
-  digitalWrite(this->pin, state);
+  digitalWrite(this->pin, (this->inverted ? !state : state));
 }
 
 void Led::turn(bool state) {
@@ -51,26 +58,41 @@ void Led::turnOn() {
   this->turn(true);
 }
 
+void Led::play() {
+  this->turnOn();
+}
+
 void Led::turnOff() {
   this->turn(false);
 }
 
+void Led::stop() {
+  this->turnOff();
+}
+
 void Led::loop() {
-  if (this->times > 0) {
+  if (this->mode == LED_BLINK_MODE_INFINITE) {
     unsigned long diff = millis() - this->lastStatus;
+
+    if (!this->lastState && diff >= this->statusOn || this->lastState && diff >= this->statusOff) {
+      this->localTurn(!this->lastState);
+    }
+  } else if (this->times > 0) {
+    unsigned long diff = millis() - this->lastStatus;
+
     if (!this->lastState && diff >= this->statusOn) {
       this->localTurn(true);
     } else if (this->lastState && diff >= this->statusOff) {
       this->localTurn(false);
       --this->times;
     }
-  } else if (this->mode == LED_BLINK_MODE) {
+  } else if (this->mode == LED_BLINK_MODE_FINITE) {
     this->turn(this->lastStateBeforeBlink);
   }
 }
 
 void Led::blink(unsigned int times, unsigned long statusOn, unsigned long statusOff) {
-  this->mode = LED_BLINK_MODE;
+  this->mode = (times == 0 ? LED_BLINK_MODE_INFINITE : LED_BLINK_MODE_FINITE);
   this->lastStateBeforeBlink = this->lastState;
 
   this->times = times;
